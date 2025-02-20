01c72ee507fa45e885e92716b7dcbc25cb9ddc75
APPLE <apple@APPLEs-MacBook-Pro.local>
2025-02-20 19:45:41 +0530

    updated file

--- min.py
+++ min.py
@@ -12,6 +12,8 @@ hands = mpHands.Hands()
 mpDraw = mp.solutions.drawing_utils # formality before using the model in order to draw the landmarks in the loop at line 27
 
 
+pTime = 0 # previous time
+cTime = 0 # current time
 
 while True:
     success, img = cap.read()
@@ -26,8 +28,34 @@ while True:
     # we can have multiple hands in the image so we need to loop through the hands and extract the them one by one
     if results.multi_hand_landmarks:
         for hand_marks in results.multi_hand_landmarks:
-            mpDraw.draw_landmarks(img, hand_marks) # draw the landmarks on the hand
+            # get the id number and get also the landmarks info(x, y, z) of the hand
+            for id, lm in enumerate(hand_marks.landmark):
+                #print(id, lm)
+                height, width, channel = img.shape
+                channel_x, channel_y = int(lm.x * width), int(lm.y * height)
+                print(id, channel_x, channel_y)
+                # if id == 0:
+                cv2.circle(img, (channel_x, channel_y), 15, (255, 0, 255), cv2.FILLED)
 
+            mpDraw.draw_landmarks(img, hand_marks, mpHands.HAND_CONNECTIONS) # draw the landmarks on the hand
+                                                   # draw the connection by using mpHands.HAND_CONNECTIONS
+    
+    # calculate the frame rate
+    cTime = time.time()
+    fps = 1/(cTime - pTime)
+    pTime = cTime
+
+    # display the frame rate on the screen
+    cv2.putText(
+        img, 
+        str(int(fps)), 
+        (10, 70), 
+        cv2.FONT_HERSHEY_PLAIN, 
+        3, 
+        (255, 0, 255), 
+        3
+    )
+    
     cv2.imshow("Image", img)
 
     cv2.waitKey(1)
